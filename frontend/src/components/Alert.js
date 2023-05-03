19fb5ebd49daad105b2a60caa38afd0a103f17ad
Bhaskar Kapri <bhaskarkapri07@gmail.com>
2023-05-03 10:30:03 +0530

    add code for the frontend

---
+++ frontend/src/components/Alert.js
@@ -0,0 +1,5 @@
+const Alert = ({ name }) => {
+  return window.alert(`${name} is already added to phonebook`);
+};
+
+export default Alert;
---
+++ frontend/src/components/Person.js
@@ -0,0 +1,8 @@
+const Person = ({ person, deletePerson }) => (
+  <li>
+    {person.name} {person.number}
+    <button onClick={() => deletePerson(person.id)}>Delete</button>
+  </li>
+)
+
+export default Person;
